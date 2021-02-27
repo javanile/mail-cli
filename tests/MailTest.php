805c2e3b6c5811296a97b372a039fe5f833a8ca4
francescobianco <info.francescobianco@gmail.com>
2021-02-27 09:43:24 +0100

    push

--- tests/MailTest.php
+++ tests/MailTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use mikehaertl\shellcommand\Command;
+
 test('example', function () {
-    expect(true)->toBeTrue();
+    $command = new Command('./mail');
+    expect($command->execute())->toBeTrue();
+    $command->getOutput();
 });
